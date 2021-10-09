4e5edf093e879b53047150ab835f2bb8b0683842
Richard A <richard.arias@msn.com>
2021-10-09 19:38:05 -0300

    changes

--- falling-sand/src/scripts/TileMap.gd
+++ falling-sand/src/scripts/TileMap.gd
@@ -8,7 +8,7 @@ export(int) var width
 export(int) var height
 
 var selected_type = Particle.TYPES.SAND
-
+var spawn_blocks: = false
 
 func _ready():
 	grid = Grid.new(Vector2(width, height))
@@ -25,7 +25,15 @@ func _ready():
 
 
 func _process(delta):
-	if Input.is_action_pressed("click"):
+	spawn_blocks = false
+	if Input.is_action_pressed("mouse_left"):
+		selected_type = Particle.TYPES.SAND
+		spawn_blocks = true
+	elif Input.is_action_pressed("mouse_right"):
+		selected_type = Particle.TYPES.WATER
+		spawn_blocks = true
+
+	if spawn_blocks:
 		var pos = (get_local_mouse_position() / TILE_SIZE).floor()
 		spawn_particle(pos)
 
@@ -60,10 +68,48 @@ func update():
 			match id:
 				Particle.TYPES.EMPTY:
 					pass
+
 				Particle.TYPES.SAND:
 					new_grid.set_cellv(update_sand(x, y), p)
+
+				Particle.TYPES.WATER:
+					new_grid.set_cellv(update_water(x, y), p)
+
 	grid = new_grid
 
+
+func update_water(x: int, y: int) -> Vector2:
+	var down = grid.get_cell(x, y + 1)
+	
+	# CASE 0: MAP LIMIT
+	if typeof(down) == TYPE_INT:
+		return Vector2(x, y)
+
+	# CASE 1: Move down
+	if not down:
+		return Vector2(x, y + 1)
+
+	var dleft = grid.get_cell(x - 1, y + 1)
+	var dright = grid.get_cell(x + 1, y + 1)
+
+	# CASE 2: Move diagonal left
+	if not down and not dleft:
+		return Vector2(x - 1, y + 1)
+	# CASE 3: Move diagonal right
+	if not down and not dright:
+		return Vector2(x + 1, y + 1)
+
+	var left = grid.get_cell(x - 2, y + 1)
+	var right = grid.get_cell(x + 2, y + 1)
+
+	if down and not left:
+		return Vector2(x - 1, y)
+
+	if down and not right:
+		return Vector2(x + 1, y)
+
+	# CASE DEFAULT
+	return Vector2(x, y)
 
 func update_sand(x: int, y: int) -> Vector2:
 	var down = grid.get_cell(x, y + 1)
